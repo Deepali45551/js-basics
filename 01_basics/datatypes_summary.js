36211a99bccb3ee16d1d55dd697b432869ccc708
Deepali Katiyar <143311713+Deepali45551@users.noreply.github.com>
2024-04-01 17:10:37 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+//console.log(id===anotherId)//false
+
+//const bigNumber=34567543432n;
+//Arrays Objects Functions
+const heroes=["shaktimaan","nargra","dore"];
+let myObj={
+name:"Deepali",
+Age:22
+}
+const myfunction=function(){
+    console.log("Hello World")
+}
+//console.log(typeof bigNumber);//undefined
+//console.log(typeof outsideTemp);//object
+console.log(typeof myfunction)//function
