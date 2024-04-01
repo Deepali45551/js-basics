6067228135e01914a88bac87dbc980d1ac4c355c
Deepali Katiyar <143311713+Deepali45551@users.noreply.github.com>
2024-04-01 13:18:50 +0000

    da

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=2031
+let accountEmail="deepalik21@google.com"
+var accountPassword="12354"
+accountCity="Jaipur"
+let accountState;
+//accountId=23 //note allowed
+accountEmail="hdsjd3@gmail.com"
+accountPassword="21223"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ;//treat all jscode as newer version
+//alert(3+3);// we re using nodejs not browser
+console.log(3+3)
+console.log("Deepali")
+
+let name="Deepali"
+let age=23
+let isLoggedIn = true
+let state=null
+//number=> 2 to powe 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined //value not assign
+//symbol => unique
+
+//object
+console.log(typeof"Deepali");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
