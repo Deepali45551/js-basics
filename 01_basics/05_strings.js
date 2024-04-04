e614a7962f9f2f62c5264b4c31bd7416c65e0d43
Deepali Katiyar <143311713+Deepali45551@users.noreply.github.com>
2024-04-04 07:12:14 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Deepali"
+const repoCount = 50
+
+ //console.log(name + repoCount + " Value");//Deepali50 Value
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Deepali-hc-com')
+
+ //console.log(gameName[0]);//D
+ //console.log(gameName.__proto__);//{}
+
+
+//console.log(gameName.length);//14
+//console.log(gameName.toUpperCase());//DEEPALI-HC-COM
+//console.log(gameName.charAt(2));// e
+//console.log(gameName.indexOf('t'));//-1
+
+const newString = gameName.substring(0, 4)
+//console.log(newString);// Deep
+
+const anotherString = gameName.slice(-6, 4)
+console.log(anotherString); //error
+
+const newStringOne = "   dipali    "
+//console.log(newStringOne);//space dipali
+//console.log(newStringOne.trim());//dipali
+
+const url = "https://dipali.com/dipali%20katiyar"
+
+//console.log(url.replace('%20', '-')) //https://dipali.com/dipali-katiyar
+
+//console.log(url.includes('sundar'))//false
+
+//console.log(gameName.split('-'));//[ 'Deepali', 'hc', 'com' ]
