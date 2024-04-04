011ac5bb757cd2553dff764653b410a156f09905
Deepali Katiyar <143311713+Deepali45551@users.noreply.github.com>
2024-04-04 08:13:40 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score=400
+const balance=new Number(100)
+//console.log(balance); //100
+
+//console.log(balance.toString().length);// 3
+//console.log(balance.toFixed(2));//100.00
+
+const otherNumber=123.8966
+//console.log(otherNumber.toPrecision(4));//123.9
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'));//10,00,000
+
+// ++++++++++++++++ Maths +++++++++++++++++//
+//console.log(Math);//object
+//console.log(Math.abs(-4));// 4
+//console.log(Math.round(4.6));// 5
+//console.log(Math.ceil(4.2));//5
+//console.log(Math.floor(4.9)); //4
+//console.log(Math.min(3,1,5,6,7));//1
+//console.log(Math.max(3,1,5,6,7));// 7
+
+//console.log(Math.random()); //between 0 and 1
+//console.log((Math.random()*10)+1); //2.25......
+//console.log(Math.floor(Math.random()*10)+1); //9
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)//above 10
+
+
+
+
