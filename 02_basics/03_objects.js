2799f10a07bd2d1e214a876d3f2afa5392901736
Deepali Katiyar <143311713+Deepali45551@users.noreply.github.com>
2024-04-05 04:59:25 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,34 @@
+//singleton
+//Object.create
+//object literals
+const mySym=Symbol("key1")
+
+const jsUser={
+    name:"Deepali",
+    "full name":"Deepali Katiyar",
+    [mySym]:"myKey1",
+    age:20,
+    loaction:"jaipur",
+    email:"deepali21@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+//console.log(jsUser.email) //deepali21@google.com
+//console.log(jsUser["email"])// deepali21@google.com
+//console.log(jsUser["full name"]);// Deepali Katiyar
+//console.log(jsUser[mySym])//myKey1
+
+jsUser.email="deepali@chat.com"// freeze value not change to microsoft
+//Object.freeze(jsUser)
+jsUser.email="deepali2microsoft.com"
+//console.log(jsUser);
+
+jsUser.greeting=function(){
+    console.log("Hello js USER");
+}
+jsUser.greetingTwo=function(){
+    console.log(`Hello js USER,${this.name}`);
+}
+//console.log(jsUser.greeting);//[Function (anonymous)] function not execute but refrence
+//console.log(jsUser.greeting());//Hello js USER
+console.log(jsUser.greetingTwo());// Hello js USER,Deepali
